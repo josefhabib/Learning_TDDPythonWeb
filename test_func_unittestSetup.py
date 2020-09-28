f36cc2a2a6090bf897fb8749dc350e0acb775e40
Josef Habib <josef.habib2@gmail.com>
2020-09-28 15:01:38 +0100

    Updating unittest file for enabling direct local run

--- test_func_unittestSetup.py
+++ test_func_unittestSetup.py
@@ -11,14 +11,21 @@ import func_unittestSetup
 class test_unittestSetupModule(unittest.TestCase):
     def test_add(self): # Syntax: test method MUST start with "test_" and receive i/p parameter 'self'
         self.assertEqual(func_unittestSetup.add(5,3), 8)
+        self.assertEqual(func_unittestSetup.add(-5,-2), -7)
+        self.assertEqual(func_unittestSetup.add(3,-3), 0)
         #NOTES: Syntax 
         # 1. 'self'.assert
         # 2. 'moduleName'.functionName
 
+    def test_mult(self): 
+        self.assertEqual(func_unittestSetup.mult(5,3), 15)
+        self.assertEqual(func_unittestSetup.mult(-5,-2), 10)
+        self.assertEqual(func_unittestSetup.mult(3,-3), -9)
 
-# NOTES: Execution
-# > Terminal:   
-#       You CANNOT simply execute this code ($python -m test_func_unittestSetup)!
-#       You must execute unittest and pass the unittest FILE as a parameter
-#
-#       $ python -m unittest test_func_unittestSetup.py
+# Allow module to be executed directly 
+# (rather than having to execute unittest with the module as a parameter)
+# > With the addition of the line below, you can now run the unit test via
+#   1. The terminal: $ python test_func_unittestSetup.py 
+#   2. By 'run'ing the local file from the IDE
+if __name__=='__main__':
+    unittest.main()
